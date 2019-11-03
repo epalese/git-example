10ea3e7769be9563d780c9cc7edda45b3fbf695f
Emanuele <leleplx@gmail.com>
2019-11-03 22:44:18 +0000

    a change to file2.py from branch2

--- file2.py
+++ file2.py
@@ -1,3 +1,3 @@
-print("This is file2.py and I modified it from master")
+print("This is file2.py and I modified it from branch2")
 print("I am introducing a change from branch1 at line 1")
-
+print("I am introducing a change from branch2 at line 1")
