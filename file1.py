cab6cd7109cd0f343f159388b99885f5c3231a2c
Emanuele <leleplx@gmail.com>
2019-11-03 17:02:27 +0000

    Commit file via Sourcetree.

--- file1.py
+++ file1.py
@@ -1,2 +1,17 @@
+import sys
+
 print("First statement")
 print("Second statement")
+print("Third statement with multiple words")
+
+
+def my_func():
+    print("sys.copyright")
+
+
+def my_func2():
+    print(sys.copyright)
+
+
+my_func()
+my_func2()
